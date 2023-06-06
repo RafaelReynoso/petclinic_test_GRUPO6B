751b3a73e01c9623c67459db41a3c11de6daf2ee
Johan Ramos <johan.ramos.m@tecsup.edu.pe>
2023-06-06 15:03:12 -0500

    crear testCreate y testFindById

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -2,9 +2,17 @@ package com.tecsup.petclinic.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.tecsup.petclinic.entities.Pet;
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.exception.PetNotFoundException;
+import com.tecsup.petclinic.exception.VetNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+@Slf4j
 class VetServiceTest {
 	
 	@Autowired
@@ -12,7 +20,18 @@ class VetServiceTest {
 
 	@Test
 	void testCreate() {
-		fail("Not yet implemented");
+		String VET_NAME = "Rocio";
+		String VET_LASTNAME = "Duarte";
+
+		Vet vet = new Vet(VET_NAME, VET_LASTNAME);
+
+		Vet vetCreated = this.VetService.create(vet);
+
+		log.info("VET CREATED :" + vetCreated.toString());
+
+		assertNotNull(vetCreated.getId());
+		assertEquals(VET_NAME, vetCreated.getName());
+		assertEquals(VET_LASTNAME,vetCreated.getLastname());
 	}
 
 	@Test
@@ -27,7 +46,18 @@ class VetServiceTest {
 
 	@Test
 	void testFindById() {
-		fail("Not yet implemented");
+		Integer ID = 1;
+		String VET_NAME = "James";
+		Vet vet = null;
+
+		try {
+			vet = this.VetService.findById(ID);
+		} catch (VetNotFoundException e) {
+			fail(e.getMessage());
+		}
+
+		log.info(vet.toString());
+		assertEquals(VET_NAME, vet.getName());
 	}
 
 	@Test

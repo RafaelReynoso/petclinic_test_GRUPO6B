a495055945c9a90a1f0e0129053bad8add53b308
Ariana Azabache <ariana.azabache@tecsup.edu.pe>
2023-06-06 15:29:24 -0500

    agregar testDelete y testFindByName

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -2,8 +2,15 @@ package com.tecsup.petclinic.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tecsup.petclinic.entities.Pet;
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.exception.PetNotFoundException;
+import com.tecsup.petclinic.exception.VetNotFoundException;
 
 class VetServiceTest {
 	
@@ -21,9 +28,33 @@ class VetServiceTest {
 	}
 
 	@Test
-	void testDelete() {
-		fail("Not yet implemented");
-	}
+	public void testDeleteVet() {
+		  String VET_NAME = "James";
+		  String VET_LASTNAME = "Carter";
+
+	        // ------------ Create ---------------
+
+	        Vet vet = new Vet(VET_NAME, VET_LASTNAME);
+	        vet = this.VetService.create(vet);
+	        log.info("" + vet);
+
+	        // ------------ Delete ---------------
+
+	        try {
+	            this.VetService.delete(vet.getId());
+	        } catch (VetNotFoundException e) {
+	            fail(e.getMessage());
+	        }
+
+	        // ------------ Validation ---------------
+
+	        try {
+	            this.VetService.findById(vet.getId());
+	            assertTrue(false);
+	        } catch (VetNotFoundException e) {
+	            assertTrue(true);
+	        }
+	        
 
 	@Test
 	void testFindById() {
@@ -31,9 +62,15 @@ class VetServiceTest {
 	}
 
 	@Test
-	void testFindByName() {
-		fail("Not yet implemented");
-	}
+	public void testFindByName() {
+		 String FIND_NAME = "Leo";
+	        int SIZE_EXPECTED = 1;
+
+	        List<Vet> vets = this.VetService.findByName(FIND_NAME);
+
+	        assertEquals(SIZE_EXPECTED, vets.size());
+	    }
+
 
 	@Test
 	void testFindByLastname() {

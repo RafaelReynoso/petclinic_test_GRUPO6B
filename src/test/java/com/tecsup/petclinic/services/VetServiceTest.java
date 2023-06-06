1ad1f832f0acfe063864da727bcc8d23b025234e
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2023-06-06 14:50:42 -0500

    agregar test (actualizacion 2)

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 class VetServiceTest {
 	
 	@Autowired
-    private PetService petService;
+    private VetService VetService;
 
 	@Test
 	void testCreate() {

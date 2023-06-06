902b63b8c36770761c402e95fc14fb9c6f5da367
JorgeTrinidadHerrera <jorge.trinidad@tecsup.edu.pe>
2023-06-06 15:32:53 -0500

    test update

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.tecsup.petclinic.entities.Vet;
+
 class VetServiceTest {
 	
 	@Autowired
@@ -17,7 +19,36 @@ class VetServiceTest {
 
 	@Test
 	void testUpdate() {
-		fail("Not yet implemented");
+		
+		
+        String VET_NAME = "Helen";
+        String VET_LASTNAME = "Leary";
+        
+        String UP_VET_NAME = "Jorge";
+        String UP_VET_LASTNAME = "Trinidad";
+
+        Vet vet = new Vet(VET_NAME, VET_LASTNAME);
+
+        // ------------ Create ---------------
+
+        log.info(">" + vet);
+        Vet vetCreated = this.VetService.create(vet);
+        log.info(">>" + vetCreated);
+
+        // ------------ Update ---------------
+
+        // Prepare data for update
+        vetCreated.setName(UP_VET_NAME);
+        vetCreated.setLastname(UP_VET_LASTNAME);
+        
+        // Execute update
+        Vet upgradeVet = this.VetService.update(vetCreated);
+        log.info(">>>>" + upgradeVet);
+
+        //            EXPECTED        ACTUAL
+        assertEquals(UP_VET_NAME, upgradeVet.getName());
+        assertEquals(UP_VET_LASTNAME, upgradeVet.getLastname());
+		
 	}
 
 	@Test

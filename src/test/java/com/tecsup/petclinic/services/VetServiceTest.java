b9f1361616c9e382b91bc044f58b1ab3716bb693
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2023-06-06 21:54:22 -0500

    agregando assertEquals a los tests

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import com.tecsup.petclinic.entities.Vet;
 import com.tecsup.petclinic.exception.VetNotFoundException;

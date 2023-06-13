8a42be7639672475bf16845216294fb2353c736b
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2023-06-13 14:55:55 -0500

    agregar testFindAllVets al VetControllerTest

--- src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -1,12 +1,45 @@
 package com.tecsup.petclinic.webs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.tecsup.petclinic.domain.VetTO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 @Slf4j
 public class VetControllerTest {
+
+    private static final ObjectMapper om = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testFindAllVets() throws Exception{
+
+        int ID_FIRST_RECORD = 1;
+
+        this.mockMvc.perform((get("/vets")))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$[0].id", is(ID_FIRST_RECORD)));
+
+    }
+
 
 }

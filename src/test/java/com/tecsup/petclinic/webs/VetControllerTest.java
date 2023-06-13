1de7ce014a15d3399047b04812fbb959a67caab7
Johan <johan.ramos.m@tecsup.edu.pe>
2023-06-13 14:24:48 -0500

    crear VetControllerTest

---
+++ src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -0,0 +1,12 @@
+package com.tecsup.petclinic.webs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Slf4j
+public class VetControllerTest {
+
+}
